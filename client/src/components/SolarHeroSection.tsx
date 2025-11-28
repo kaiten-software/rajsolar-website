dbe8a68ebd587ecd8a288f1b4b448962744aa758
kaiten-software <kaiten-software@users.noreply.github.com>
2025-11-28 22:51:31 +0530

    Fix scroll indicator z-index to appear above location badge

--- client/src/components/SolarHeroSection.tsx
+++ client/src/components/SolarHeroSection.tsx
@@ -173,7 +173,7 @@ export default function SolarHeroSection() {
       </div>
 
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce z-20">
         <div className="w-6 h-10 border-2 border-green-600 rounded-full flex items-start justify-center p-2">
           <div className="w-1 h-3 bg-green-600 rounded-full"></div>
         </div>
